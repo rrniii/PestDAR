89b8d97bc0c12865466e090c615bec8e214b4885
rrniii <rrniii@me.com>
2020-11-06 08:42:27 +0000

    Add exists check clause to Oman_Plot_Raw_Data_Script.py

--- notebooks/Oman_Plot_Raw_Data_Script.py
+++ notebooks/Oman_Plot_Raw_Data_Script.py
@@ -40,85 +40,89 @@ def main():
 
         fig_save_file_path = plotpath + loc_folder + ele_folder + R_folder
 
-        # Figure Options
-        width = 30  # in inches
-        height = 10  # in inches
+        if os.path.isfile(fig_save_file_path + plot_file_name):
+            print(fig_save_file_path + plot_file_name + ' exists')
+        else:
 
-        fig = plt.figure(figsize=(width, height))
-        nrows = 2
-        ncols = 5
+            # Figure Options
+            width = 30  # in inches
+            height = 10  # in inches
 
-        ax1 = fig.add_subplot(nrows, ncols, 1)
-        display.plot('dBZ', ele, ax=ax1, vmin=-32, vmax=16., title='Horizontal Reflectivity',
-                     colorbar_label=radar.fields['dBZ']['units'],
-                     axislabels=('', 'North South distance from radar (km)'))
-        display.set_limits((-R, R), (-R, R), ax=ax1)
+            fig = plt.figure(figsize=(width, height))
+            nrows = 2
+            ncols = 5
 
-        ax2 = fig.add_subplot(nrows, ncols, 2)
-        display.plot('ZDR', ele, ax=ax2, vmin=-2, vmax=10., title='Differential Reflectivity',
-                     colorbar_label=radar.fields['ZDR']['units'],
-                     axislabels=('', ''), cmap='pyart_RefDiff')
-        display.set_limits((-R, R), (-R, R), ax=ax2)
+            ax1 = fig.add_subplot(nrows, ncols, 1)
+            display.plot('dBZ', ele, ax=ax1, vmin=-32, vmax=16., title='Horizontal Reflectivity',
+                         colorbar_label=radar.fields['dBZ']['units'],
+                         axislabels=('', 'North South distance from radar (km)'))
+            display.set_limits((-R, R), (-R, R), ax=ax1)
 
-        ax3 = fig.add_subplot(nrows, ncols, 3)
-        display.plot('RhoHV', ele, ax=ax3, vmin=0, vmax=1., title='Cross Correlation Ratio',
-                     colorbar_label=radar.fields['RhoHV']['units'],
-                     axislabels=('', ''), cmap='pyart_RefDiff')
-        display.set_limits((-R, R), (-R, R), ax=ax3)
+            ax2 = fig.add_subplot(nrows, ncols, 2)
+            display.plot('ZDR', ele, ax=ax2, vmin=-2, vmax=10., title='Differential Reflectivity',
+                         colorbar_label=radar.fields['ZDR']['units'],
+                         axislabels=('', ''), cmap='pyart_RefDiff')
+            display.set_limits((-R, R), (-R, R), ax=ax2)
 
-        ax4 = fig.add_subplot(nrows, ncols, 4)
-        display.plot('KDP', ele, ax=ax4, vmin=-2, vmax=5., title='Specific Differential Phase',
-                     colorbar_label=radar.fields['KDP']['units'],
-                     axislabels=('', ''), cmap='pyart_Theodore16')
-        display.set_limits((-R, R), (-R, R), ax=ax4)
+            ax3 = fig.add_subplot(nrows, ncols, 3)
+            display.plot('RhoHV', ele, ax=ax3, vmin=0, vmax=1., title='Cross Correlation Ratio',
+                         colorbar_label=radar.fields['RhoHV']['units'],
+                         axislabels=('', ''), cmap='pyart_RefDiff')
+            display.set_limits((-R, R), (-R, R), ax=ax3)
 
-        ax5 = fig.add_subplot(nrows, ncols, 5)
-        display.plot('PhiDP', ele, ax=ax5, vmin=-180, vmax=180., title='Differential Phase',
-                     colorbar_label=radar.fields['PhiDP']['units'],
-                     axislabels=('', ''), cmap='pyart_Wild25')
-        display.set_limits((-R, R), (-R, R), ax=ax5)
+            ax4 = fig.add_subplot(nrows, ncols, 4)
+            display.plot('KDP', ele, ax=ax4, vmin=-2, vmax=5., title='Specific Differential Phase',
+                         colorbar_label=radar.fields['KDP']['units'],
+                         axislabels=('', ''), cmap='pyart_Theodore16')
+            display.set_limits((-R, R), (-R, R), ax=ax4)
 
-        ax6 = fig.add_subplot(nrows, ncols, 6)
-        display.plot('V', ele, ax=ax6, vmin=-16, vmax=16., title='Doppler Velocity',
-                     colorbar_label=radar.fields['V']['units'],
-                     axislabels=('East West distance from radar (km)', 'North South distance from radar (km)'),
-                     cmap='pyart_BuDRd18')
-        display.set_limits((-R, R), (-R, R), ax=ax6)
+            ax5 = fig.add_subplot(nrows, ncols, 5)
+            display.plot('PhiDP', ele, ax=ax5, vmin=-180, vmax=180., title='Differential Phase',
+                         colorbar_label=radar.fields['PhiDP']['units'],
+                         axislabels=('', ''), cmap='pyart_Wild25')
+            display.set_limits((-R, R), (-R, R), ax=ax5)
 
-        ax7 = fig.add_subplot(nrows, ncols, 7)
-        display.plot('W', ele, ax=ax7, vmin=0, vmax=5., title='Spectrum Width',
-                     colorbar_label=radar.fields['W']['units'],
-                     axislabels=('East West distance from radar (km)', ''), cmap='pyart_NWS_SPW')
-        display.set_limits((-R, R), (-R, R), ax=ax7)
+            ax6 = fig.add_subplot(nrows, ncols, 6)
+            display.plot('V', ele, ax=ax6, vmin=-16, vmax=16., title='Doppler Velocity',
+                         colorbar_label=radar.fields['V']['units'],
+                         axislabels=('East West distance from radar (km)', 'North South distance from radar (km)'),
+                         cmap='pyart_BuDRd18')
+            display.set_limits((-R, R), (-R, R), ax=ax6)
 
-        ax8 = fig.add_subplot(nrows, ncols, 8)
-        display.plot('SNR', ele, ax=ax8, title='SNR', colorbar_label=radar.fields['SNR']['units'],
-                     axislabels=('East West distance from radar (km)', ''), cmap='pyart_Carbone17')
-        display.set_limits((-R, R), (-R, R), ax=ax8)
+            ax7 = fig.add_subplot(nrows, ncols, 7)
+            display.plot('W', ele, ax=ax7, vmin=0, vmax=5., title='Spectrum Width',
+                         colorbar_label=radar.fields['W']['units'],
+                         axislabels=('East West distance from radar (km)', ''), cmap='pyart_NWS_SPW')
+            display.set_limits((-R, R), (-R, R), ax=ax7)
 
-        ax9 = fig.add_subplot(nrows, ncols, 9)
-        display.plot('SQI', ele, ax=ax9, title='SQI', colorbar_label=radar.fields['SQI']['units'],
-                     axislabels=('East West distance from radar (km)', ''), cmap='pyart_Carbone17')
-        display.set_limits((-R, R), (-R, R), ax=ax9)
+            ax8 = fig.add_subplot(nrows, ncols, 8)
+            display.plot('SNR', ele, ax=ax8, title='SNR', colorbar_label=radar.fields['SNR']['units'],
+                         axislabels=('East West distance from radar (km)', ''), cmap='pyart_Carbone17')
+            display.set_limits((-R, R), (-R, R), ax=ax8)
 
-        ax10 = fig.add_subplot(nrows, ncols, 10)
-        display.plot('uPhiDP', ele, ax=ax10, vmin=-360, vmax=360., title='Unfiltered Differential Phase',
-                     colorbar_label=radar.fields['uPhiDP']['units'],
-                     axislabels=('East West distance from radar (km)', ''), cmap='pyart_Wild25')
-        display.set_limits((-R, R), (-R, R), ax=ax10)
+            ax9 = fig.add_subplot(nrows, ncols, 9)
+            display.plot('SQI', ele, ax=ax9, title='SQI', colorbar_label=radar.fields['SQI']['units'],
+                         axislabels=('East West distance from radar (km)', ''), cmap='pyart_Carbone17')
+            display.set_limits((-R, R), (-R, R), ax=ax9)
 
-        display.plot_cross_hair(3.)
-        plt.suptitle(title, fontsize=24)
+            ax10 = fig.add_subplot(nrows, ncols, 10)
+            display.plot('uPhiDP', ele, ax=ax10, vmin=-360, vmax=360., title='Unfiltered Differential Phase',
+                         colorbar_label=radar.fields['uPhiDP']['units'],
+                         axislabels=('East West distance from radar (km)', ''), cmap='pyart_Wild25')
+            display.set_limits((-R, R), (-R, R), ax=ax10)
 
-        if not os.path.exists(fig_save_file_path):
-            os.makedirs(fig_save_file_path)
+            display.plot_cross_hair(3.)
+            plt.suptitle(title, fontsize=24)
 
-        plt.savefig(fig_save_file_path + plot_file_name, dpi=100)
+            if not os.path.exists(fig_save_file_path):
+                os.makedirs(fig_save_file_path)
 
-        plt.close()
+            plt.savefig(fig_save_file_path + plot_file_name, dpi=100)
 
-        del radar
-        del display
+            plt.close()
+
+            del radar
+            del display
 
     data_dir = '/Volumes/Neely/PestDAR/Oman_Raw_Netcdf_Radar_Data/sur/'
     plotpath = '/Volumes/Neely/PestDAR/Oman_Raw_Data_Plots/'
